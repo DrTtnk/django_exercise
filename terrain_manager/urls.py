a9d60252715f42d687e2c8bd629894bd40d8e58e
DrTtnk <pietrobelgioco@gmail.com>
2021-06-25 12:54:40 +0200

    Adding CRUD

--- terrain_manager/urls.py
+++ terrain_manager/urls.py
@@ -2,8 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get_all',         views.get_all, name='terrains-get-all'),
-    path('get/<int:id>',    views.get,     name='terrains-get'),
-    path('update/<int:id>', views.update,  name='terrains-update'),
-    path('delete/<int:id>', views.destroy, name='terrains-destroy'),
+    path('terrain/get_all',         views.get_all_terrains, name='terrains-get-all'),
+    path('terrain/get/<int:id>',    views.get_terrain,      name='terrains-get'),
+    path('terrain/create/<int:id>', views.create_terrain,   name='terrains-create'),
+    path('terrain/update/<int:id>', views.update_terrain,   name='terrains-update'),
+    path('terrain/delete/<int:id>', views.destroy_terrain,  name='terrains-destroy'),
+
+    path('sensor/get_all',         views.get_all_sensors, name='sensors-get-all'),
+    path('sensor/get/<int:id>',    views.get_sensor,      name='sensors-get'),
+    path('sensor/create/<int:id>', views.create_sensor,   name='sensors-create'),
+    path('sensor/update/<int:id>', views.update_sensor,   name='sensors-update'),
+    path('sensor/delete/<int:id>', views.destroy_sensor,  name='sensors-destroy'),
 ]
--- terrain_manager/views.py
+++ terrain_manager/views.py
@@ -1,29 +1,78 @@
-from django.http import JsonResponse
+import json
+
+from django.http import JsonResponse, HttpRequest
 from django.shortcuts import redirect
-from .models import Terrain
+from .models import Terrain, User, Sensor
 from django.forms.models import model_to_dict
 
 
-def get_all(request):
-    return JsonResponse({
-        'terrains': [model_to_dict(t) for t in Terrain.objects.all()]
-    })
+def get_all_terrains(request: HttpRequest):
+    return JsonResponse({'terrains': [model_to_dict(t) for t in Terrain.objects.all()]})
 
 
-def get(request, id):
-    return JsonResponse({
-        'terrain': model_to_dict(Terrain.objects.get(id=id))
-    })
+def get_terrain(request: HttpRequest, id):
+    return JsonResponse({'terrain': model_to_dict(Terrain.objects.get(id=id))})
 
 
-def update(request, id):
+def create_terrain(request: HttpRequest):
+    terrain = Terrain.objects.create()
+    json_data = json.loads(request.body)
+
+    terrain.owner = User.objects.get(json_data['owner'])
+    terrain.name = json_data['name']
+    terrain.description = json_data['description']
+    terrain.position = json_data['position']
+
+    return JsonResponse({'terrain': model_to_dict(terrain)})
+
+
+def update_terrain(request: HttpRequest, id):
     terrain = Terrain.objects.get(id=id)
-    terrain.name = request
-    return JsonResponse({
-        'terrain': model_to_dict(terrain)
-    })
+    json_data = json.loads(request.body)
+
+    terrain.name = json_data['name']
+    terrain.description = json_data['description']
+    terrain.position = json_data['position']
+
+    return JsonResponse({'terrain': model_to_dict(terrain)})
 
 
-def destroy(request, id):
+def destroy_terrain(request: HttpRequest, id):
     Terrain.objects.get(id=id).delete()
-    return redirect("/show")
+    return redirect("/get_all")
+
+
+def get_all_sensors(request: HttpRequest):
+    return JsonResponse({'sensors': [model_to_dict(t) for t in Sensor.objects.all()]})
+
+
+def get_sensor(request: HttpRequest, id):
+    return JsonResponse({'sensor': model_to_dict(Sensor.objects.get(id=id))})
+
+
+def create_sensor(request: HttpRequest):
+    sensor = Sensor.objects.create()
+    json_data = json.loads(request.body)
+
+    sensor.name = Terrain.objects.get(json_data['owner'])
+    sensor.terrain = json_data['terrain']
+    sensor.description = json_data['description']
+    sensor.position = json_data['position']
+
+    return JsonResponse({'sensor': model_to_dict(sensor)})
+
+
+def update_sensor(request: HttpRequest, id):
+    sensor = Sensor.objects.get(id=id)
+    json_data = json.loads(request.body)
+
+    sensor.name = json_data['name']
+    sensor.description = json_data['description']
+    sensor.position = json_data['position']
+
+    return JsonResponse({'sensor': model_to_dict(sensor)})
+
+
+def destroy_sensor(request: HttpRequest, id):
+    Sensor.objects.get(id=id).delete()
+    return redirect("/get_all")
